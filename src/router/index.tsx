00cbeab4e8ffc0f2d4337be8393f86d6194bd00e
Mikael Messias <mikaelmessias@outlook.com>
2020-06-20 17:36:27 -0300

    Add Home page
    Signed-off-by: Mikael Messias <mikaelmessias@outlook.com>

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,11 @@
 import React, { FunctionComponent } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from 'pages/Home';
 
 const Router: FunctionComponent = () => (
   <BrowserRouter>
     <Switch>
-      <Route path="/" exact />
+      <Route path="/" component={Home} exact />
     </Switch>
   </BrowserRouter>
 );
